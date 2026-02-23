fe3c20524fd4d8582f7cd22888f88b5630a13ffd
bolivartorrent <adrianjoserodriguezvidal@gmail.com>
2026-02-23 12:59:24 -0600

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,15 +1,23 @@
-from telegram.ext import Updater, CommandHandler
+from telegram.ext import Updater, MessageHandler, Filters
 
-# Reemplaza 'TU_TOKEN' con el token que te dio BotFather
+# Reemplaza con el token de tu bot
 TOKEN = '7852494996:AAGxQpNUj8ckm94GKBFYb0BjLnGOcpJQCjU'
 
 def start(update, context):
     update.message.reply_text('¡Hola! Soy tu bot de Telegram.')
 
+def echo(update, context):
+    update.message.reply_text('Recibí tu mensaje: ' + update.message.text)
+
 def main():
     updater = Updater(TOKEN, use_context=True)
     dp = updater.dispatcher
-    dp.add_handler(CommandHandler("start", start))
+
+    # Comando /start
+    dp.add_handler(MessageHandler(Filters.command, start))
+    # Responder a cualquier mensaje
+    dp.add_handler(MessageHandler(Filters.text & ~Filters.command, echo))
+    
     updater.start_polling()
     updater.idle()
 
